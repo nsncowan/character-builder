f2375562a7e4e46e9e9af192b77421137c1728da
nsncowan <nsncowan@gmail.com>
2022-08-11 14:50:36 -0700

    add html and rest of js to code
    Co-authored-by: Spencer Dennis <sjdennis2113@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,51 @@
 //Co-authored-by: Spencer Dennis <sjdennis2113@gmail.com>"
 //const type = document.querySelector("barbarian").value;
 
+//business logic
+
+let barbarian = document.getElementById("barbarian-stats");
+let cleric = document.getElementById("cleric-stats");
+let ranger = document.getElementById("ranger-stats");
+let paladin = document.getElementById("paladin-stats");
+
 
 //User Interface Logic
-
-const name = document.querySelector("input#nameinput").value; 
-const selection = document.querySelector("type").value;
+function hideResults() {
+  document.getElementById("barbarian-stats").setAttribute("class", "hidden");
+  document.getElementById("cleric-stats").setAttribute("class", "hidden");
+  document.getElementById("ranger-stats").setAttribute("class", "hidden");
+  document.getElementById("paladin-stats").setAttribute("class", "hidden");
+}
 
 function nameoffunction(event) {
   event.preventDefault();
   hideResults();
-    const selection = document.querySelector("type").value;
+    const barbarian = document.getElementById("barbarian-stats").value;
+    const cleric = document.getElementById("cleric-stats").value;
+    const ranger = document.getElementById("ranger-stats").value;
+    const paladin = document.getElementById("paladin-stats").value;
 
-    if (type === barbarian) {
+    console.log(barbarian + cleric + ranger + paladin)
+    const c = document.getElementById("type").value;
+    console.log(c)
+
+    if (c === "Barbarian") {
         document.getElementById("barbarian-stats").removeAttribute("class");
-      } else if (type === cleric) {
+        console.log("if statement")
+      } 
+      else if (c === "cleric") {
         document.getElementById("cleric-stats").removeAttribute("class");
-      } else if (type === ranger) {
+      } else if (c === "ranger") {
         document.getElementById("ranger-stats").removeAttribute("class");
       }
-      else if (type === paladin) {
+      else if (c === "paladin") {
         document.getElementById("paladin-stats").removeAttribute("class");
       }
+      console.log("did it run")
   }
 
-  window.onload = function() {
-    document.querySelector("form").addEventListener("submit", nameoffunction)  
-  }
+  window.addEventListener("load", function() {
+    document.getElementById("character-type").addEventListener("submit", nameoffunction);
+    console.log("page load function worked")
+  });
 
-//business logic
-
-let barbarian = document.getElementById("barbarian-stats");
-barbarian.setAttribute("class", "hidden");
-
-let cleric = document.getElementById("cleric-stats");
-cleric.setAttribute("class", "hidden");
-
-let ranger = document.getElementById("ranger-stats");
-ranger.setAttribute("class", "hidden");
-
-let paladin = document.getElementById("paladin-stats");
-paladin.setAttribute("class", "hidden");
